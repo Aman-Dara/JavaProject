7c7667f603bcd50fac23d56ecd0042aa9057537d
Aman-Dara <amandeep0992@gmail.com>
2023-07-12 17:01:46 -0400

    Revisied Code

--- src/loopAssignment/CheckTankCapacity.java
+++ src/loopAssignment/CheckTankCapacity.java
@@ -2,21 +2,42 @@ package loopAssignment;
 
 public class CheckTankCapacity {
 
-	int currentWaterLevel = 7; // assuming there is already 7L water in the tank
-	int amountToBeFilledInLitres = 10;
-	int maxCapacityInLitres = 97; // assuming max capacity to 97L
+	double currentWaterLevel = 0.25;
+	int bucketVolume = 10;
+	int maxCapacityOfTank = 95; // assuming max capacity to 95L
+	int tankCapacity = 100;
+	double waterRemaining;
 
 	void fillTankToCapacity() {
 
-		while (currentWaterLevel <= maxCapacityInLitres) {
-			System.out.println("The water in tank is " + currentWaterLevel + "L");
-			currentWaterLevel += amountToBeFilledInLitres;
+		while (maxCapacityOfTank < tankCapacity) {
 
-			if (currentWaterLevel == maxCapacityInLitres) {
-				System.out.println("You have reached the maximum Capacity!!");
-				break;
+			// if there is water in the tank
+			if (currentWaterLevel != 0 && currentWaterLevel <= maxCapacityOfTank) {
+				System.out.println("Water level in tank " + currentWaterLevel + "L");
+				waterRemaining = maxCapacityOfTank - currentWaterLevel;
+				currentWaterLevel += bucketVolume;
+				System.out.println("You need " + waterRemaining + "L of more water to reach the max capacity");
+
+				if (waterRemaining == 0) {
+					System.out.println("You have reached the max capacity!");
+					break;
+				}
 			}
 
+			// if there is no water in the tank
+			if (currentWaterLevel == 0 && currentWaterLevel <= maxCapacityOfTank) {
+				System.out.println("Water level in tank " + currentWaterLevel + "L");
+				waterRemaining = maxCapacityOfTank - currentWaterLevel;
+				System.out.println("You need " + waterRemaining + "L of more water to reach the max capacity");
+				currentWaterLevel += bucketVolume;
+				System.out.println("You need " + waterRemaining + "L of more water to reach the max capacity");
+
+				if (waterRemaining == 0) {
+					System.out.println("You have reached the max capacity!");
+					break;
+				}
+			}
 		}
 	}
 }
